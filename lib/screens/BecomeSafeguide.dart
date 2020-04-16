8093b1eae11de6f27ca3d5d31007280358c55456
hargun79 <hargunsinghsahni@gmail.com>
2020-04-16 19:51:39 +0530

    Updated Safeguide feature.

--- lib/screens/BecomeSafeguide.dart
+++ lib/screens/BecomeSafeguide.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+String name;
+String dob;
+String pNumber;
+String email;
+String location;
 
 class BecomeSafeguide extends StatelessWidget {
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final TextEditingController _controller1 = new TextEditingController();
+  final TextEditingController _controller2 = new TextEditingController();
+  final TextEditingController _controller3 = new TextEditingController();
+  final TextEditingController _controller4 = new TextEditingController();
+  final TextEditingController _controller5 = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Become a Safeguide'),
         backgroundColor: Colors.blueAccent,
@@ -29,52 +39,91 @@ class BecomeSafeguide extends StatelessWidget {
                 ),
               ),
               new TextFormField(
+                controller: _controller1,
                 decoration: const InputDecoration(
                   icon: const Icon(Icons.person),
                   hintText: 'Enter your first and last name',
                   labelText: 'Name',
                 ),
+                onChanged: (value) {
+                  name = value;
+                },
               ),
               new TextFormField(
+                controller: _controller2,
                 decoration: const InputDecoration(
                   icon: const Icon(Icons.calendar_today),
                   hintText: 'Enter your date of birth',
                   labelText: 'Dob',
                 ),
+                onChanged: (value) {
+                  dob = value;
+                },
                 keyboardType: TextInputType.datetime,
               ),
               new TextFormField(
+                controller: _controller3,
                 decoration: const InputDecoration(
                   icon: const Icon(Icons.phone),
                   hintText: 'Enter a phone number',
                   labelText: 'Phone',
                 ),
+                onChanged: (value) {
+                  pNumber = value;
+                },
                 keyboardType: TextInputType.phone,
                 inputFormatters: [
                   WhitelistingTextInputFormatter.digitsOnly,
                 ],
               ),
               new TextFormField(
+                controller: _controller4,
                 decoration: const InputDecoration(
                   icon: const Icon(Icons.email),
                   hintText: 'Enter a email address',
                   labelText: 'Email',
                 ),
+                onChanged: (value) {
+                  email = value;
+                },
                 keyboardType: TextInputType.emailAddress,
               ),
               new TextFormField(
+                controller: _controller5,
                 decoration: const InputDecoration(
                   icon: const Icon(Icons.location_on),
                   hintText: 'Enter area',
                   labelText: 'Location',
                 ),
+                onChanged: (value) {
+                  location = value;
+                },
               ),
               new Container(
                 padding: const EdgeInsets.only(left: 40.0, top: 20.0),
                 child: new RaisedButton(
                   child: const Text('Submit'),
                   onPressed: () {
-                    _displaySnackBar(context);
+                    _mailto() async {
+                      String url =
+                          'mailto:wecare2020@gmail.com?subject=Become%20a%20safeguide&body=Name:%20$name%0D%0ADOB:%20$dob%0D%0APhone%20Number:%20$pNumber%0D%0AEmail:%20$email%0D%0ALocation:%20$location';
+                      print("test url1");
+                      if (await canLaunch(url)) {
+                        print("test url2");
+                        await launch(url);
+                      } else {
+                        print("test url3");
+                        throw 'Could not launch $url';
+                      }
+                    }
+
+                    _controller1.clear();
+                    _controller2.clear();
+                    _controller3.clear();
+                    _controller4.clear();
+                    _controller5.clear();
+                    _mailto();
+                    Navigator.pop(context);
                   },
                 ),
               ),
@@ -83,11 +132,5 @@ class BecomeSafeguide extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  _displaySnackBar(BuildContext context) {
-    final snackBar =
-        SnackBar(content: Text('SafeGuide Registered Succesfully.'));
-    _scaffoldKey.currentState.showSnackBar(snackBar);
   }
 }
